0a6d134d446f8acbdd14495c7b04b02c6d3948d4
AndrewLaird <alaird@uci.edu>
2020-01-07 20:52:40 -0800

    walker start

---
+++ walker.py
@@ -0,0 +1,126 @@
+import gym
+import collections
+import torch
+from torch import nn
+
+def run_game(env,model=False,render=False,max_frames=10000):
+    obs = env.reset()
+    frames = 0
+    done = False
+
+    experience = []
+    while(not done and frames < max_frames):
+        if(model):
+            action = model.predict(obs)
+        else:
+            action = env.action_space.sample()
+            print(action)
+
+        new_obs,reward,done,info = env.step(action)
+
+        if(render):
+            env.render()
+
+        frames += 1
+
+        experience.append([obs,action,new_obs,reward])
+
+        obs = new_obs
+
+
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        nn.init.normal_(m.weight, mean=0., std=0.1)
+        nn.init.constant_(m.bias, 0.1)
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, num_inputs, num_outputs, hidden_size, std=0.0):
+        # Pytorch Setup of a module
+        super(ActorCritic, self).__init__()
+
+        self.critic = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 1)
+        )
+
+        self.actor = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, num_outputs),
+        )
+        self.log_std = nn.Parameter(torch.ones(1, num_outputs) * std)
+
+        self.apply(init_weights)
+
+    def forward(self, x):
+        value = self.critic(x)
+        mu    = self.actor(x)
+        std   = self.log_std.exp().expand_as(mu)
+        dist  = Normal(mu, std)
+        return dist, value
+    
+
+def compute_gae(next_value, rewards, masks, values, gamma=0.99, tau=0.95):
+    values = values + [next_value]
+    gae = 0
+    returns = []
+    for step in reversed(range(len(rewards))):
+        delta = rewards[step] + gamma * values[step + 1] * masks[step] - values[step]
+        gae = delta + gamma * tau * masks[step] * gae
+        returns.insert(0, gae + values[step])
+    return returns
+
+def ppo_iter(mini_batch_size, states, actions, log_probs, returns, advantage):
+    batch_size = states.size(0)
+    for _ in range(batch_size // mini_batch_size):
+        rand_ids = np.random.randint(0, batch_size, mini_batch_size)
+        yield states[rand_ids, :], actions[rand_ids, :], log_probs[rand_ids, :], returns[rand_ids, :], advantage[rand_ids, :]
+
+
+
+def ppo_update(ppo_epochs, mini_batch_size, states, actions, log_probs, returns, advantages, clip_param=0.2):
+    for _ in range(ppo_epochs):
+        for state, action, old_log_probs, return_, advantage in ppo_iter(mini_batch_size, states, actions, log_probs, returns, advantages):
+            dist, value = model(state)
+            entropy = dist.entropy().mean()
+            new_log_probs = dist.log_prob(action)
+
+            ratio = (new_log_probs - old_log_probs).exp()
+            surr1 = ratio * advantage
+            surr2 = torch.clamp(ratio, 1.0 - clip_param, 1.0 + clip_param) * advantage
+
+            actor_loss  = - torch.min(surr1, surr2).mean()
+            critic_loss = (return_ - value).pow(2).mean()
+
+            loss = 0.5 * critic_loss + actor_loss - 0.001 * entropy
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+if (__name__ == "__main__"):
+    env = gym.make("BipedalWalker-v2")
+
+    num_inputs  = envs.observation_space.shape[0]
+    num_outputs = envs.action_space.shape[0]
+
+    model = ActorCritic(num_inputs, num_outputs, hidden_size).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    max_frames = 1000000
+    frame_idx  = 0
+    test_rewards = []
+
+
+    # play 5 games to get started
+    run_game(env,render=True)
+
+    model
+
+
+    
+
+
